5a596ae52b2733248c1f562a9993ef0be289da9a
Alan Szmyt <szmyty@gmail.com>
2025-05-20 14:11:51 -0400

    Add Keycloak authentication

--- apps/ui/vite-project/src/main.tsx
+++ apps/ui/vite-project/src/main.tsx
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+import keycloak from './keycloak';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+keycloak.onTokenExpired = () => {
+  keycloak.updateToken(30).catch(() => keycloak.login());
+};
+
+keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+  if (!authenticated) {
+    keycloak.login();
+    return;
+  }
+
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  );
+});
